72caf7c5cbe1d10540fb329fb8308d55904b4710
Ellen Booth <70690003+ellenbooth@users.noreply.github.com>
2023-10-06 09:12:17 +0100

    Create InvestmentsController.php

---
+++ InvestmentsController.php
@@ -0,0 +1,22 @@
+//// rest of the file above
+
+/**
+ * @param CreateInvestmentRequest $request
+ * @param CreateInvestmentService $service
+ *
+ * @return Response
+ */
+public function createInvestment(CreateInvestmentRequest $request, CreateInvestmentService $service): Response
+{
+    try {
+        $service->createRepair(
+            $request->post('accountId'),
+            $request->post('fundId'),
+            $request->post('amount'),
+            new DateTimeImmutable($request->post('created')),
+        );
+        return new Response(null, 204);
+    } catch (Exception $e) {
+        return new Response(['status' => 'error', 'message' => $exception->getMessage()], 500);
+    }
+}
