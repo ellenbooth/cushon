6c7293356ffaeb92a8a94fdc258a7f4af766c95a
Ellen Booth <70690003+ellenbooth@users.noreply.github.com>
2023-10-07 16:07:53 +0100

    Update FundClientFactory.php

--- FundClientFactory.php
+++ FundClientFactory.php
@@ -1,1 +1,25 @@
+/// stuff up here 
 
+class FundClientFactory
+{
+    /**
+     * @param Fund $fund
+     * @return FundClientInterface
+     * @throws FundsException
+     */
+    public function create(Fund $fund): FundClientInterface
+    {
+        switch ($fund->getBroker()) {
+            case 'XTB':
+                return app(XTBClient::class);
+          
+            case 'eToro':
+                return app(MultiMtaService::class);
+          
+            case 'FinecoBank':
+                return app(ChangePremisesService::class);  
+        }
+
+        throw FundsException::BrokerClientNotFound($fund->getbroker());
+    }
+}
